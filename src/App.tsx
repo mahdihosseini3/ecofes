c51ed3f9143fdfb93455e909568a9e7a4e099669
Puri <poorya.comsanjari@gmail.com>
2025-04-12 13:11:41 +0330

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className={""}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus commodi magni necessitatibus reprehenderit
+      sit veniam. Ab autem dicta eaque est ex harum non, quidem saepe sequi ullam voluptatem, voluptatibus?
+    </div>
+  )
+}
+
+export default App
