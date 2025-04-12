76378f2d4636fab81cdd0fc9b57243ed2dc8455d
Puri <poorya.comsanjari@gmail.com>
2025-04-12 13:40:47 +0330

    add component folder

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className={""}>
-      hello world
+
     </div>
   )
 }
---
+++ src/components/ui/HeroSection.tsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+  return (
+    <div className={""}>
+
+    </div>
+  );
+};
+
+export default HeroSection;
